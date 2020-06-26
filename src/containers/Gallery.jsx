1ac1582aa3d70433cf9046fd460b4ca899984118
Leandro <quenochetete1120@gmail.com>
2020-06-26 11:12:52 -0300

    changed system to api

--- src/containers/Gallery.jsx
+++ src/containers/Gallery.jsx
@@ -1,118 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/index.css";
 import Credential from "../assets/credentials";
-import Controls from "../containers/Controls.jsx";
+import Loader from "./Loader.jsx";
 
-const url_pixa = `https://pixabay.com/api/?key=${Credential.url_key}&`;
+const Gallery = () => {
+  const url_pixa = `https://pixabay.com/api/?key=${Credential.url_key}&`;
+  const [url, setURL] = useState(url_pixa);
+  const [image, setImage] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
-class Gallery extends React.Component {
-  state = {
-    images: {
-      hits: [],
-    },
-    load: true,
-    error: null,
-    url: url_pixa + "per_page=20",
-    page: 1,
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.hits);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  });
+
+  const nextPage = () => {
+    setPage(page + 1);
+    setURL(url_pixa + `page=${page}`);
   };
 
-  componentDidMount() {
-    this.fetchImages();
-  }
+  return (
+    <>
+      <button onClick={nextPage}>Load More</button>
 
-  componentWillUpdate() {
-    this.fetchImages();
-  }
-
-  fetchImages = async () => {
-    try {
-      const res = await fetch(this.state.url);
-      const data = await res.json();
-      this.setState({
-        load: false,
-        images: {
-          hits: data.hits,
-        },
-      });
-      console.log(this.state.images);
-    } catch (error) {
-      this.setState({
-        load: false,
-        error: error,
-      });
-    }
-  };
-
-  urlChanger = (c) => {
-    const url = url_pixa + `${c}`;
-    console.log(url);
-    this.setState({
-      url: url,
-      page: 1 + 1,
-    });
-    this.componentWillUpdate();
-  };
-
-  render() {
-    return (
-      <>
-        <input
-          type="button"
-          value="backgrounds"
-          onClick={() => this.urlChanger("category=backgrounds")}
-        />
-        <input
-          type="button"
-          value="nature"
-          onClick={() => this.urlChanger("nature")}
-        />
-        <input
-          type="button"
-          value="fashion"
-          onClick={() => this.urlChanger("fashion")}
-        />
-        <input
-          type="button"
-          value="science"
-          onClick={() => this.urlChanger("science")}
-        />
-        <input
-          type="button"
-          value="education"
-          onClick={() => this.urlChanger("education")}
-        />
-        <input
-          type="button"
-          value="health"
-          onClick={() => this.urlChanger("health")}
-        />
-        <input
-          type="button"
-          value="music"
-          onClick={() => this.urlChanger("music")}
-        />
-        <input
-          type="button"
-          value="animals"
-          onClick={() => this.urlChanger("animals")}
-        />
-
-        <div className="gallery-image">
-          {this.state.images.hits.map((i) => (
-            <div className="gallery-item">
-              <img className="img" src={i.webformatURL} alt={i.type} />
-            </div>
-          ))}
-        </div>
-
-        <input
-          type="button"
-          value="Next ->"
-          onClick={() => this.urlChanger(`page=${this.state.page + 1}`)}
-        />
-      </>
-    );
-  }
-}
+      <div className="gallery-image">
+        {isLoading && <Loader />}
+        {image.map((i) => (
+          <div key={i.id} className="gallery-item">
+            <img className="img" src={i.webformatURL} alt={i.type} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Gallery;
